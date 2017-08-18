bef59a863aa254facf3ddfafd7709c69d68e8b52
Sangjun Park <huad126@naver.com>
2017-08-18 23:04:46 +0900

    12를 출력하는 다양한 방법

---
+++ Print12.java
@@ -0,0 +1,11 @@
+class Print12
+{
+  public static void main(String[] args)
+   {
+     System.out.println(12);
+     System.out.println("1"+"2");
+     System.out.println(1*12);
+     System.out.println(10+2);
+     System.out.println(1+"2");
+     }
+}
